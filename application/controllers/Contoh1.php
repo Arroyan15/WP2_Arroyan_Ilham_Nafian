0ae92116358e1cf4e48f196b41d0649e791c2abd
arroyan <arroyannafian@gmail.com>
2023-10-06 08:22:54 +0700

    first init github

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Arroyan Ilham Nafian
+		Saya tinggal di daerah Cilendek
+		olah raga yang saya sukai adalah GYM";
+	}
+}
---
+++ application/controllers/homepage.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homepage Arroyan</title>
+    <link rel="stylesheet" href="style.css"> <!-- Buat file CSS -->
+</head>
+<body>
+    <header>
+        <h2>Selamat Datang di Homepage Saya</h2>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">Beranda</a></li>
+            <li><a href="#">Tentang Saya</a></li>
+            <li><a href="#">Kontak</a></li>
+        </ul>
+    </nav>
+
+    <section class="content">
+        <h1>Ini Adalah Gambar Saya</h1>
+        <img src="vtuber.png" width="300" height="200">
+    </section>
+
+    <footer>
+        <p>&copy; <?php echo date("Y"); ?> Homepage.php</p>
+    </footer>
+</body>
+</html>
